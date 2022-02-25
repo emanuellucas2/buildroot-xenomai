0945c7e0cb30d9ec243fc2ed86885332f194455f
emanuellucas2 <emanuellucas2@yahoo.com.br>
2022-02-24 22:22:11 -0300

    Trying 3.0 Xenomai

--- custom-scripts/post-build.sh
+++ custom-scripts/post-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-tar -xvf $BASE_DIR/../custom-scripts/packages/xenomai-3.1.tar.bz2 -C $BASE_DIR/build
-pushd $BASE_DIR/build/xenomai-3.1
+tar -xvf $BASE_DIR/../custom-scripts/packages/xenomai-3.0.tar.bz2 -C $BASE_DIR/build
+pushd $BASE_DIR/build/xenomai-3.0
 scripts/bootstrap
 export CFLAGS="-march=armv7-a -mfloat-abi=hard -mfpu=neon -ffast-math"
 export LDFLAGS="-march=armv7-a -mfloat-abi=hard -mfpu=neon -ffast-math"
@@ -14,3 +14,6 @@ popd
 
 make -C $BASE_DIR/../custom-scripts/latency_app
 cp $BASE_DIR/../custom-scripts/latency_app/latency $BASE_DIR/target/usr/xenomai/bin
+
+echo -e >> $BASE_DIR/target/etc/profile
+echo -e "export LD_LIBRARY_PATH=/usr/xenomai/lib" >> $BASE_DIR/target/etc/profile
